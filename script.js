dba1b11c5f7fb59740309a3ad77245976d8edbe5
ericmargay <ericmargay@gmail.com>
2022-10-11 17:38:32 -0500

    boton funcional de copy

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
-var copied = document.getElementById("copied")
-var btn = document.getElementById("btn")
+const btnCopy = document.querySelector('.btn-copy');
+const txtCopy = document.querySelector('.link');
 
-btn.addEventListener("click", () => {
-    var copy = document.getElementById("copy")
+btnCopy.addEventListener('click', () => {
+  navigator.clipboard.writeText(txtCopy.innerText);
+});
 
-    copy.select()
-    copy.setSelectionRange(0, 99999)
-    document.execCommand("copy")
-
-    copied.style.top = "-55px"
-    copied.style.opacity = "1"
-    setTimeout(() => {
-        copied.style.top = "-40px"
-        copied.style.opacity = "0"
-    }, 1000);
-})
