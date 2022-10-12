740bd5f61a03ec8f7c8f98dc6e1cfe1822f26f55
ericmargay <ericmargay@gmail.com>
2022-10-12 14:23:02 -0500

    se encontró el index

--- script.js
+++ script.js
@@ -1,24 +1,55 @@
 const btnCopy = document.querySelectorAll('.btn-copy');
 const txtCopy = document.querySelectorAll('.link');
 
-console.log(btnCopy)
-
-btnCopy.forEach(btnCopy_f => {
-
-    btnCopy_f.addEventListener('click', function (index) {
-
-
-        console.log('Se presiono el' + index )
+btnCopy.forEach((btnCopy_f, index) => {
+	btnCopy_f.addEventListener('click', function handleClick() {
+		console.log(index);
+		console.log(txtCopy.innerText);
 
 
 
-        // btnCopy_f.clipboard.writeText(txtCopy.innerText);
 
-    })
 
+        // btnCopy_f.clipboard.writeText(txtCopy.innerText[index]);
+
+
+	})
 });
 
-txtCopy.forEach(function (element, index, array) {
-  console.log("element = " + element, "index = " + index, "array = " + array);
+
+
+
+// btnCopy.forEach(box => {
+//   box.addEventListener('click', function handleClick(event) {
+//     console.log('box clicked', event);
+
+//     box.setAttribute('style', 'background-color: salmon;');
+//   });
+// });
+
+
+// const btnCopy = document.querySelectorAll('.btn-copy');
+// 
+
+// console.log(btnCopy)
+
+// btnCopy.forEach(btnCopy_f => {
+
+//     btnCopy_f.addEventListener('click', function (index) {
+
+
+//         console.log('Se presiono el' + index.target)
+
+
+
+
+//     })
+
+// });
+
+
+
+// txtCopy.forEach(function (element, index, array) {
+//   console.log("element = " + element, "index = " + index, "array = " + array);
   
-});
+// });
