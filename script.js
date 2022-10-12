72baaac0c24dbe528bae1560b7315e2e28be8466
ericmargay <ericmargay@gmail.com>
2022-10-12 12:37:41 -0500

    codigo no terminado pero para continuar la busqueda de index

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-const btnCopy = document.querySelector('.btn-copy');
-const txtCopy = document.querySelector('.link');
+const btnCopy = document.querySelectorAll('.btn-copy');
+const txtCopy = document.querySelectorAll('.link');
 
-btnCopy.addEventListener('click', () => {
-  navigator.clipboard.writeText(txtCopy.innerText);
+console.log(btnCopy)
+
+btnCopy.forEach(btnCopy_f => {
+
+    btnCopy_f.addEventListener('click', function (index) {
+
+
+        console.log('Se presiono el' + index )
+
+
+
+        // btnCopy_f.clipboard.writeText(txtCopy.innerText);
+
+    })
+
 });
 
+txtCopy.forEach(function (element, index, array) {
+  console.log("element = " + element, "index = " + index, "array = " + array);
+  
+});
