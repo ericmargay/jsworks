7fa055ebee642f1d58adc68792fea1a87f4ae8b9
ericmargay <ericmargay@gmail.com>
2022-10-13 12:57:03 -0500

    python to json

--- excel.py
+++ excel.py
@@ -1,7 +1,14 @@
-#instalar pandas (pip install pandas)
-#instalar xlrd (pip install xlrd)
-#instalar openpyxl (pip install openpyxl)
-import pandas as pd
 
-archivo=pd.read_excel('Registros.xlsx') #lectura de archivo
-print(archivo)
+import pandas as pd
+import json
+
+
+excel_data_df = pd.read_excel('Registros.xlsx')
+
+json_str = excel_data_df.to_json()
+
+print(json_str)
+
+
+with open("excel.json", "w") as outfile:
+    json.dump(json_str, outfile)
