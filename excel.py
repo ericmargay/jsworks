d1b84e1401391ef16940f259f44d58753be43b53
SergioP_eum <sergio.palapa.o@gmail.com>
2022-10-13 12:17:11 -0500

    lectura excel con pyton

---
+++ excel.py
@@ -0,0 +1,7 @@
+#instalar pandas (pip install pandas)
+#instalar xlrd (pip install xlrd)
+#instalar openpyxl (pip install openpyxl)
+import pandas as pd
+
+archivo=pd.read_excel('Registros.xlsx') #lectura de archivo
+print(archivo)
